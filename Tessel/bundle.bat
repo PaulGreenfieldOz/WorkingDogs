996131c25dee6bde531093ab00f3d995cba40616
Paul Greenfield <38850780+PaulGreenfieldOz@users.noreply.github.com>
2018-11-06 15:53:22 +1100

    Blue tessel (#9)
    * 17-Aug-2018
    
    * Updates to Tessel
    
    Added various pre-compiled versions of Tessel, also updated Kelpie ReadMe

--- Tessel/bundle.bat
+++ Tessel/bundle.bat
@@ -1,6 +1,10 @@
 REM https://download.mono-project.com/runtimes/raw/
 
 mkdir OSX-10.7
+<<<<<<< HEAD
+mkdir ubuntu-14.04
+=======
+>>>>>>> master
 mkdir ubuntu-16.04
 mkdir ubuntu-18.04
 mkdir debian-8
@@ -8,6 +12,10 @@ mkdir debian-9
 mkdir Windows
 
 call mkbundle -o OSX-10.7\Tessel Tessel.exe --simple --cross mono-5.14.0-osx-10.7-x64
+<<<<<<< HEAD
+call mkbundle -o ubuntu-14.04\Tessel Tessel.exe --simple --cross mono-5.14.0-ubuntu-14.04-x64
+=======
+>>>>>>> master
 call mkbundle -o ubuntu-16.04\Tessel Tessel.exe --simple --cross mono-5.14.0-ubuntu-16.04-x64
 call mkbundle -o ubuntu-18.04\Tessel Tessel.exe --simple --cross mono-5.14.0-ubuntu-18.04-x64
 call mkbundle -o debian-8\Tessel Tessel.exe --simple --cross mono-5.14.0-debian-8-x64

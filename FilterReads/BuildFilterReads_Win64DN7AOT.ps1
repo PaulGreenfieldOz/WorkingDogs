b4c4feee284e188cbbe90ffabf42499611f4827c
Paul Greenfield <gre403@csiro.au>
2022-10-02 20:05:37 +1100

    FilterReads (.NET)
    .NET port plus changes to fuzzy matching and pair-matching

---
+++ FilterReads/BuildFilterReads_Win64DN7AOT.ps1
@@ -0,0 +1,3 @@
+dotnet publish .\FilterReads.csproj -c release /p:PublishProfile=Win64DN7AOTFolderProfile.pubxml
+mkdir Win64DN7AOT
+copy bin\Release\net7.0\publish\win-x64\FilterReads.exe .\Win64DN7AOT\
